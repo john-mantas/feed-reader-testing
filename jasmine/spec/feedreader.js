6b16db6006166f823fcc42d899297017a9818b50
John Mantas <mantasjohn67@gmail.com>
2018-05-30 20:58:27 +0300

    feat: Add tests for the menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,23 +54,37 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+    /* Test suite named "The menu" */
+    describe('The menu', function() {
+        /* Test that ensures the menu element is hidden by
+         * default. 
          */
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toEqual(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
+        /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+        it('changes visibility on clicks', function() {
+//            var menuXaxis = $('.slide-menu')[0].getBoundingClientRect().x;
+            
+            //Open the menu
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBeFalsy();
+//            expect(menuXaxis).toBe(0);  //why this isn't working?
+            //Hide the menu
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBeTruthy();
+//            expect(menuXaxis).not.toBe(0);
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
+    /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -79,7 +93,7 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
